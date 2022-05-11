feeb1c97f8cfc6a2c664e525dd85f60835c4baf8
Thinhtv <thinhtv.dev@gmail.com>
2022-05-11 16:10:35 +0700

    update front-end

---
+++ mainindex.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage LoginSystems</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="home-index">
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>LOGIN</h1>
+            <form action="" class="form-signin">
+                <input type="text" placeholder="username/email" name="user_uid">
+                <input type="password" placeholder="password" name="user_pwd">
+                <button type="submit">Login</button>
+                <a href="signup.php" class="btn btn-red signup">Register</a>
+            </form>
+        </nav>
+    <section class="main-body">
+        <h3>Sign Up</h3>
+        <div class="content">
+        <form action="">
+            <input type="text" placeholder="First Name..." name="user-first">
+            <input type="text" placeholder="Last Name..." name="user-last">
+            <input type="email" placeholder="Email" name="user_email">
+            <input type="text" placeholder="Username" name="user_uid">
+            <input type="password" placeholder="Password" name="user_pwd">
+            <button type="submit" class="btn btn-submit">Sign Up</button> 
+        </form>
+        </div>
+    </section> 
+    <footer class="footer">
+        <p> Design by <a href="#">Thinh Tran</a></p>
+    </footer>
+    </div>
+    
+</body>
+</html>
